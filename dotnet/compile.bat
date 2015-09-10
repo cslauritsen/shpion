5213c6642d08d242d52c0ff4794287dd23145c7b
Chad S. Lauritsen <chad.s.lauritsen@sherwin.com>
2015-09-10 10:41:37 -0400

    removed test example program

--- dotnet/compile.bat
+++ dotnet/compile.bat
@@ -1,4 +1,4 @@
 set csc=C:\Windows\Microsoft.NET\Framework64\v4.0.30319\csc.exe 
 
 %csc% /reference:M2Mqtt.Net.dll SWService.cs 
-%csc% /reference:M2Mqtt.Net.dll RegistryExample.cs
+rem %csc% /reference:M2Mqtt.Net.dll RegistryExample.cs
